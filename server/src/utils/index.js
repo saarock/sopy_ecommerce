3fb81640eb4a29b2b8cfdfc794a586e8909c757a
saarock <saarock4646@gmail.com>
2026-01-06 23:49:48 +0545

    removed un-necessary codes from the server [backend]

--- server/src/utils/index.js
+++
@@ -1,6 +0,0 @@
-export { default as asyncHandler } from "./asyncHandler.js";
-export { default as ApiError } from "./apiError.js";
-export { default as ApiResponse } from "./apiResponse.js";
-export { default as NodeMailSender } from "./mailSender.js";
-export {default as mailOtpStore} from "./mailOtpStore.js";
-
